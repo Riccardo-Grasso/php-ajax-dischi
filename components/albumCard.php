a2be22c6a22b68f5a6e53b8c61b52b6c0241246b
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-12-01 18:20:21 +0100

    cards_php

--- components/albumCard.php
+++ components/albumCard.php
@@ -1,6 +1,8 @@
-<div class="card align-items-center">
-    <img src="<?php echo $albumImg ?>" class="card-img-top" alt="image" />
-    <div class="card-body">
-        <?php echo $albumData ?>
+<div class="col">
+    <div class="card align-items-center">
+        <img src="<?php echo $albumImg ?>" class="card-img-top" alt="image" />
+        <div class="card-body">
+            <?php echo $albumData ?>
+        </div>
     </div>
 </div>
--- components/main_php.php
+++ components/main_php.php
@@ -4,7 +4,7 @@
 
  <main>
      <div class="container">
-         <div class="row row-cols-5 pt-5">
+         <div class="row row-cols-5 pt-5 g-4">
              <?php
                 foreach ($albums as $album) {
                     $albumImg = $album["poster"];
