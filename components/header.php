8567c6a5ead6e286f096cbdb5b8cd0e15ab006fa
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-12-01 15:47:43 +0100

    start

---
+++ components/header.php
@@ -0,0 +1,5 @@
+<header>
+    <div class="navbar container-fluid d-flex align-items-center">
+        <img src="./assets/spotify-2.svg" alt="logo" />
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+require __DIR__ . "/components/pageHead.php"
+?>
+
+<body>
+    <!-- HEADER -->
+    <?php
+    include_once  "components/header.php"
+    ?>
+
+    <!-- MAIN -->
+    <?php
+    include_once  "components/main.php"
+    ?>
+</body>
+
+</html>
